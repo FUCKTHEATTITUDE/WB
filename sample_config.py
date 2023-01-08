1e63b2468d4642aea8b18a4706f570843a2c53aa
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2023-01-08 16:32:24 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -42,7 +42,7 @@ else:
     MESSAGE_DUMP_CHAT = -1001657538863
     WELCOME_DELAY_KICK_SEC = 300
     MONGO_URL = "mongodb+srv://ZAID:ZAID@cluster0.8smkd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-    ARQ_API_KEY = "JGAGCN-IRAJKY-PTPEXD-FCDGHX-ARQ"
+    ARQ_API_KEY = "IDYNEY-SLKMLF-YMTCWU-TTTRIW-ARQ"
     ARQ_API_URL = "https://arq.hamker.in"
     LOG_MENTIONS = True
     RSS_DELAY = 300  # In seconds
